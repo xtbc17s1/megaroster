d42add6672e8595bd8c24fbb26002fa5a43a9c68
Dave Strus <dave@getfretless.com>
2017-05-17 14:46:13 -0400

    Setup submit handler for the new student form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation()
+
+const megaroster = {
+  init() {
+    document
+      .querySelector('#new-student')
+      .addEventListener('submit', this.addStudent)
+  },
+
+  addStudent(ev) {
+    ev.preventDefault()
+    const studentName = ev.target.studentName.value
+    console.log(studentName)
+  },
+}
+megaroster.init()
