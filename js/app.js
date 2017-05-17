35c6e6dc704b0dc6677d6493582e00302f65fe42
Dave Strus <dave@getfretless.com>
2017-05-17 12:09:32 -0400

    Remove students from the ul, but not the array

--- js/app.js
+++ js/app.js
@@ -6,9 +6,21 @@ const megaroster = {
   init(listSelector) {
     this.studentList = document.querySelector(listSelector)
     this.max = 0
+    this.setupEventListeners()  
+  },
+
+  setupEventListeners() {
     document
       .querySelector('#new-student')
       .addEventListener('submit', this.addStudent.bind(this))
+  },
+
+  removeStudent(ev) {
+    const btn = ev.target
+    btn.closest('.student').remove()
+
+    // Remove it from the this.students array
+    // this.students.splice(?, 1)
   },
 
   addStudent(ev) {
@@ -37,6 +49,10 @@ const megaroster = {
     this.removeClassName(li, 'template')
     li.querySelector('.student-name').textContent = student.name
     li.dataset.id = student.id
+
+    li
+      .querySelector('button.remove')
+      .addEventListener('click', this.removeStudent.bind(this))
     return li
   },
 
