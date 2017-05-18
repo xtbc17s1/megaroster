1380443f5be176bcf6bc1b828035e31e02eac39b
Dave Strus <dave@getfretless.com>
2017-05-18 14:15:42 -0400

    Load students from localStorage on page load

--- js/app.js
+++ js/app.js
@@ -7,29 +7,43 @@ const megaroster = {
     this.studentList = document.querySelector('#student-list')
     this.max = 0
     this.setupEventListeners()
+    this.load()
   },
 
   setupEventListeners() {
     document
       .querySelector('#new-student')
-      .addEventListener('submit', this.addStudent.bind(this))
+      .addEventListener('submit', this.addStudentViaForm.bind(this))
   },
 
-  addStudent(ev) {
+  load() {
+    const rosterString = localStorage.getItem('roster')
+    const rosterArray = JSON.parse(rosterString)
+    if (rosterArray) {
+      rosterArray
+        .reverse()
+        .map(this.addStudent.bind(this))
+    }
+  },
+
+  addStudentViaForm(ev) {
     ev.preventDefault()
     const f = ev.target
     const student = {
       id: this.max + 1,
       name: f.studentName.value,
     }
+    this.addStudent(student)
+    f.reset()
+  },
 
+  addStudent(student) {
     this.students.unshift(student)
     
     const listItem = this.buildListItem(student)
     this.prependChild(this.studentList, listItem)
     
     this.max ++
-    f.reset()
     this.save()
   },
 
