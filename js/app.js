7dd0175e211b3d7a04541f44e9d7b2a59dc6cb7a
Dave Strus <dave@getfretless.com>
2017-05-22 13:38:11 -0400

    Implement "move down"

--- js/app.js
+++ js/app.js
@@ -80,6 +80,10 @@ class Megaroster {
     li
       .querySelector('button.move-up')
       .addEventListener('click', this.moveUp.bind(this, student))
+
+    li
+      .querySelector('button.move-down')
+      .addEventListener('click', this.moveDown.bind(this, student))
   }
 
   save() {
@@ -100,6 +104,24 @@ class Megaroster {
       const previousStudent = this.students[index - 1]
       this.students[index - 1] = student
       this.students[index] = previousStudent
+      this.save()
+    }
+  }
+
+  moveDown(student, ev) {
+    const btn = ev.target
+    const li = btn.closest('.student')
+
+    const index = this.students.findIndex((currentStudent, i) => {
+      return currentStudent.id === student.id
+    })
+
+    if (index < this.students.length - 1) {
+      this.studentList.insertBefore(li.nextSibling, li)
+      
+      const nextStudent = this.students[index + 1]
+      this.students[index + 1] = student
+      this.students[index] = nextStudent
       this.save()
     }
   }
