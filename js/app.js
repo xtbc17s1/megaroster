efad2c1f3db7f543912b2ab8f40acc7ad1962a5d
Dave Strus <dave@getfretless.com>
2017-05-22 09:30:51 -0400

    Edit existing student names

--- js/app.js
+++ js/app.js
@@ -117,6 +117,26 @@ class Megaroster {
     li
       .querySelector('button.move-down')
       .addEventListener('click', this.moveDown.bind(this, student))
+
+    li
+      .querySelector('[contenteditable]')
+      .addEventListener('blur', this.updateName.bind(this, student))
+
+    li
+      .querySelector('[contenteditable]')
+      .addEventListener('keypress', this.saveOnEnter.bind(this))
+  }
+
+  saveOnEnter(ev) {
+    if (ev.keyCode === 13) {
+      ev.preventDefault()
+      ev.target.blur()
+    }
+  }
+
+  updateName(student, ev) {
+    student.name = ev.target.textContent
+    this.save()
   }
 
   moveUp(student, ev) {
